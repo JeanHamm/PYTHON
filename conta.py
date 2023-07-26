6efa47764e9a28a5fc7cdbf4e7b0d13c3da09db9
Jean Hamm <jeanmichehamm@gmail.com>
2023-07-26 13:28:17 -0300

    V2

--- conta.py
+++ conta.py
@@ -5,29 +5,63 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
 
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
-    def get_saldo(self):
+    @property
+    def titular(self):
+        return self.__titular
+    #Get __saldo
+    @property
+    def saldo(self):
         return self.__saldo
-    def get_limite(self):
+    
+    #limites get and set
+    @property
+    def limite(self):
         return self.__limite
-    
-    
-    def set_limite(self, limite):
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+    
+    @staticmethod
+    def codigo_dos_bancos():
+        return {"BB":"001", "Caixa":"013", "Stone":"197"}
+    
     
     def deposita(self, valor):
         self.__saldo += valor
 
-    def saca(self, valor):
-        self.__saldo -= valor    
-
-    def transferir(self, destino, valor):
-        if(self.__saldo >= valor):
-            self.saca(valor)
-            destino.deposita(valor)
-        else:
-            print("\nEssa conta não tem o valor suficiente para transferir\n")
     
+    #Sacar o dinheiro
+    def saca(self, valor):
+        if(self.__pode_sacar(valor) == True):
+            self.__saldo -= valor
+        else:
+           print("Você não tem saldo suficiente")
+    
+    def __pode_sacar(self, valor):
+        if(valor <= self.__saldo):
+            return True
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def transferir(self, destino, valor):
+        if(valor <= self.__limite):
+            self.saca(valor)
+        else:
+            print('Não deu certo, valor insuficiente')
+        destino.deposita(valor)
--- estudos.py
+++ estudos.py
@@ -1,15 +1,17 @@
 from conta import Conta
 
-conta1 = Conta( 123, "Jean", 100.00, 2500.00)
+conta1 = Conta( 123, "jean", 100.00, 2500.00)
 conta2 = Conta( 321, "Nico", 0, 45034.00)
 conta3 = Conta( 425, "Steffany", 1020.00, 12011.00)
 
-
-
-conta1.transferir(conta2, 100.01)
-
 conta1.extrato()
-conta2.extrato()
+conta1.saca(50.00)
+conta1.extrato()
+conta1.saca(50.00)
+conta1.extrato()
+conta1.saca(50.00)
+conta1.extrato()
 
-conta3.set_limite(250.00)
-print(conta3.get_limite())
+codigos = Conta.codigo_dos_bancos()
+
+print(codigos["Stone"])

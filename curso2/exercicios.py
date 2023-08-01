cd0db6190689d3a4b167c531220cf47edcb43f9b
Jean Hamm <jeanmichehamm@gmail.com>
2023-08-01 11:22:24 -0300

    Casa 01-08-23

--- curso2/exercicios.py
+++
@@ -1,9 +0,0 @@
-from modelo import Filme, Serie
-
-filme1 = Filme("vingadores guerra infinita", 2021, 160)
-serie1 = Serie("Doctor Who", 1960, 2)
-
-
-print(f'\nNome é {filme1.nome} do ano de {filme1.ano} e tem {filme1.duracao} min, atingiu a marca de {filme1.likes} likes\n')
-
-print(f'Nome é {serie1.nome} do ano de {serie1.ano} e tem {serie1.temporadas} temporadas, atingiu a marca de {serie1.likes} likes\n')
--- curso2/testes.py
+++ curso2/testes.py
@@ -12,24 +12,23 @@ class Programa:
         return self._likes
     def dar_likes(self):
         self._likes += 1
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} L')
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min -  {self._likes} L')
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas -  {self._likes} L')
 
-    
+class Documentario(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
 vingadores = Filme("vingadores guerra infinita", 2021, 160)
 star_wars = Filme('star wars o retorno do jedi', 1968, 320)
 doctor_who = Serie("Doctor Who", 1960, 2)
@@ -38,4 +37,5 @@ the_office  = Serie('the office', 2008, 10)
 filmes_e_series = [vingadores, doctor_who, star_wars, the_office]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} - {programa.likes}')
